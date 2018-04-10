2d4e7d276511de4abb4ddaf4e401f005b256d76b
jhonatan <jdsjhonatan@hotmail.com>
2018-04-10 17:32:38 -0300

    added new vehicle from

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -4,32 +4,38 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
+import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { MakeService } from './services/make.service';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent
-    ],
-    imports: [
-        CommonModule,
-        HttpModule,
-        FormsModule,
-        RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
-        ])
-    ]
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    CounterComponent,
+    FetchDataComponent,
+    HomeComponent,
+    VehicleFormComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: "", redirectTo: "home", pathMatch: "full" },
+      { path: "vehicles/new", component: VehicleFormComponent },
+      { path: "home", component: HomeComponent },
+      { path: "counter", component: CounterComponent },
+      { path: "fetch-data", component: FetchDataComponent },
+      { path: "**", redirectTo: "home" }
+    ])
+  ],
+  providers:[
+    MakeService
+  ]
 })
-export class AppModuleShared {
-}
+export class AppModuleShared {}
