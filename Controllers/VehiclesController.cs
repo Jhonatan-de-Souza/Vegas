95f3ea0082ca85d6458f73a1f5a09856bfeb7354
jhonatan <jdsjhonatan@hotmail.com>
2018-04-13 17:18:34 -0300

    added Update Action and custom mapping for vehicleResource

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Vegas.Controllers.Resources;
 using Vegas.Models;
 using Vegas.Persistence;
@@ -22,8 +24,29 @@ namespace Vegas.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateVehicleAsync([FromBody]VehicleResource vehicleResource)
         {
+            if(!ModelState.IsValid)
+            return BadRequest(ModelState);
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            vehicle.LastUpdate = DateTime.Now;
             context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id,[FromBody]VehicleResource vehicleResource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+            if(vehicle == null){
+                return BadRequest(ModelState);
+            }
+            mapper.Map<VehicleResource, Vehicle>(vehicleResource,vehicle);
+            vehicle.LastUpdate = DateTime.Now;
+
             await context.SaveChangesAsync();
 
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using Vegas.Controllers.Resources;
@@ -9,20 +10,32 @@ namespace Vegas.Mapping
     {
         public MappingProfile()
         {
-            //Domain to API Resources
+            // Domain to API Resources
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Features, FeaturesResource>();
             CreateMap<Vehicle, VehicleResource>()
               .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource { Name = v.ContactName, Email = v.ContactEmail, Phone = v.ContactPhone })).ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => vf.FeatureId)));
 
-            //API Resources to Domain
-            //this is how you map objects when they have different nams
+            // API Resources to Domain
+            // This is how you map objects when they have different nams
             CreateMap<VehicleResource, Vehicle>()
+            .ForMember(v => v.Id,opt => opt.Ignore())
             .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
             .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
             .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
-            .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id })));
+            .ForMember(v => v.Features, opt => opt.Ignore())
+            .AfterMap((vr,v) => {
+                // Remove unselected feature
+                var removedFeatures =  v.Features.Where(f =>  !vr.Features.Contains(f.FeatureId));
+                foreach(var f in removedFeatures)
+                    v.Features.Remove(f);
+
+                // Add new features
+                 var addedFeatures = vr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id)).Select(id => new VehicleFeature{FeatureId = id});
+                foreach (var f in addedFeatures)
+                    v.Features.Add(f);
+            });
         }
     }
 }
