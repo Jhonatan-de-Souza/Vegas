5f5df91aa963d98a8b6e51129a2fa8a032f93e80
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2018-04-17 11:41:46 -0300

    Added two way binding to form elements

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -13,24 +13,30 @@ export class VehicleFormComponent implements OnInit {
   models: any[];
   features:any[];
   vehicle:any = {
-    features:[]
+  features:[],
+  contact:{}
   };
   constructor(private vehicleService:VehicleService) { }
 
   ngOnInit() {
+    /*Initializing the properties from the respective services*/
     this.vehicleService.getMakes().subscribe(makes => (this.makes = makes));
     this.vehicleService.getFeatures().subscribe(features => (this.features = features));
   }
   onMakeChange(){
+    /*Binding secondary actions (dropdown in this case) based on change event*/
     var selectedMake = this.makes.find( m => m.id == this.vehicle.makeId)
     this.models = selectedMake.models;
-    delete this.vehicle.modelId;
+    delete this.vehicle.modelId; /* removing previously selected item */
   }
   onFeatureToggle(featureId,$event){
+    /* Using javascript to obtain the selected checkbox */
     if($event.target.checked)
+    /* pushing(adding) the selected item to the angular object */
     this.vehicle.features.push(featureId);
     else{
       var index = this.vehicle.features.indexOf(featureId);
+      this.vehicle.features.splice(index,1);
     }
   }
 }
