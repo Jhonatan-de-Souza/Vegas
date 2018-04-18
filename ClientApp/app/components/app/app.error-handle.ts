fbef8cfa6bd2d56402157b9ae70246c38636789b
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2018-04-18 15:38:25 -0300

    after disabling pre-rendering

--- ClientApp/app/components/app/app.error-handle.ts
+++ ClientApp/app/components/app/app.error-handle.ts
@@ -1,19 +1,23 @@
-import { ToastyService } from 'ng2-toasty';
-import { ErrorHandler,Inject } from "@angular/core";
+import { ToastyService } from "ng2-toasty";
+import { ErrorHandler, Inject, NgZone } from "@angular/core";
 
 export class AppErrorHandler implements ErrorHandler {
-         constructor(
-             @Inject(ToastyService) private toastyService: ToastyService) {}
-         handleError(error: any): void {
-             
-             if (typeof(window) !== 'undefined') {
-           this.toastyService.error({
-             title: "Error title here",
-             msg: "This is a new message",
-             theme: "bootstrap",
-             showClose: true,
-             timeout: 5000
-           });
-         }
-        }
-       }
+  constructor(
+    @Inject(NgZone) private ngZone: NgZone,
+    @Inject(ToastyService) private toastyService: ToastyService
+  ) {}
+
+  handleError(error: any): void {
+    this.ngZone.run(() => {
+      if (typeof window !== "undefined") {
+        this.toastyService.error({
+          title: "Error title here",
+          msg: "This is a new message",
+          theme: "bootstrap",
+          showClose: true,
+          timeout: 5000
+        });
+      }
+    });
+  }
+}
