8d979e3b91563f5453b2db69fe7e75a9dce5d367
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2018-04-19 14:06:50 -0300

    implementing editing pt2

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,9 +1,13 @@
+import * as _ from 'underscore';
 import { isDevMode } from "@angular/core";
 import { FetchDataComponent } from "./../fetchdata/fetchdata.component";
 import { VehicleService } from "./../../services/vehicle.service";
 import { Component, OnInit } from "@angular/core";
 import { ToastyService } from "ng2-toasty";
 import { ActivatedRoute, Router } from "@angular/router";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/observable/forkJoin";
+import { SaveVehicle, Vehicle } from "../../models/vehicle";
 
 @Component({
   selector: "app-vehicle-form",
@@ -14,35 +18,59 @@ export class VehicleFormComponent implements OnInit {
   makes: any[];
   models: any[];
   features: any[];
-  vehicle: any = {
+  vehicle: SaveVehicle = {
+    id: 0,
+    makeId: 0,
+    modelId: 0,
+    isRegistered: false,
     features: [],
-    contact: {}
+    contact: {
+      name: "",
+      email: "",
+      phone: ""
+    }
   };
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private vehicleService: VehicleService,
-    private toastyService: ToastyService) {
-      route.params.subscribe(p => {
-        if(p['id']) 
-         this.vehicle.id = +p['id'];
-      });
+    private toastyService: ToastyService
+  ) {
+    route.params.subscribe(p => {
+      if (p["id"]) this.vehicle.id = +p["id"];
+    });
   }
 
   ngOnInit() {
-    /*Initializing the properties from the respective services*/
-    // if(this.vehicle.id > 0){
-    // this.vehicleService.getVehicle(this.vehicle.id)
-    // .subscribe(v =>{ this.vehicle =v
-    // })};
-    this.vehicleService.getVehicle(this.vehicle.id)
-      .subscribe(v => {
-        this.vehicle = v;
-    });
-    this.vehicleService.getMakes().subscribe(makes => (this.makes = makes));
-    this.vehicleService
-      .getFeatures()
-      .subscribe(features => (this.features = features));
+    /* Here we are setting up our data sources */
+    var sources = [
+      this.vehicleService.getMakes(),
+      this.vehicleService.getFeatures()
+    ];
+
+    if (this.vehicle.id)
+      sources.push(this.vehicleService.getVehicle(this.vehicle.id));
+
+    Observable.forkJoin(sources).subscribe(
+      data => {
+        this.makes = data[0];
+        this.features = data[1];
+
+        if (this.vehicle.id) this.setVehicle(data[2]);
+      },
+      err => {
+        if (err.status == 404) this.router.navigate(["/home"]);
+      }
+    );
+  }
+  private setVehicle(v: Vehicle) {
+    this.vehicle.id = v.id;
+    this.vehicle.makeId = v.make.id;
+    this.vehicle.modelId = v.model.id;
+    this.vehicle.isRegistered = v.isRegistered;
+    this.vehicle.contact = v.contact;
+    /* Using _ (underscore) is the easiest way to acess */
+    this.vehicle.features = _.pluck(v.features,'id')
   }
   onMakeChange() {
     /*Binding secondary actions (dropdown in this case) based on change event*/
