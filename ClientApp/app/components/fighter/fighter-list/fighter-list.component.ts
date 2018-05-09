b1d69ba99fd6ad6ced76c7f84fbec58a64c2b383
annon <annon@MrRobot.localdomain>
2018-05-09 10:04:17 -0300

    Implemented simple fighter listing

--- ClientApp/app/components/fighter/fighter-list/fighter-list.component.ts
+++ ClientApp/app/components/fighter/fighter-list/fighter-list.component.ts
@@ -1,3 +1,4 @@
+import { platformDynamicServer } from '@angular/platform-server';
 import { Component, OnInit } from "@angular/core";
 import { FighterService } from "../../../services/fighter.service";
 import { Fighter } from "./../../../models/fighter";
@@ -5,28 +6,37 @@ import { Fighter } from "./../../../models/fighter";
 @Component({
     selector: "app-fighter-list",
     // templateUrl: "./fighter-list.component.html"
-    template: `<h1>This is the inline title</h1>
+    template: `
 
-    <div *ngFor="let f of fighters" >
-    <label for="fighters{{ f.id }}">
-      {{ f.name }}
-    </label>
-  </div>
+    <h1> Fighter Listing</h1>
+    <table class="table table-condensed table-hover">
+    <thead>
+        <tr>
+            <th>Id<th>
+            <th>Name<th>
+            <th>Power<th>
+            <th>Speed<th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr *ngFor="let fighter of fighters">
+            <td> {{fighter.id}}<td>
+            <td> {{fighter.name}}<td>
+            <td> {{fighter.power}}<td>
+            <td> {{fighter.speed}}<td>
+        </tr>
+    </tbody>            
+    </table>
     `
-    
+
 })
 export class FighterListComponent implements OnInit {
     fighters: Fighter[];
-    constructor(private fighteService: FighterService){}
 
-    ngOnInit(){
-        
-         
+    constructor(private fighteService: FighterService) { }
+
+    ngOnInit() {
         this.fighteService.getFighters()
             .subscribe(fighters => this.fighters = fighters);
-
-        this.fighteService.getFighters()
-            .subscribe(fighters => console.log(fighters));
-
     }
 }
